194aaf86162070c5b2fe28f78de7e3ec934c12d4
Lucas Nachtigall <lucas.nachtigall@gmail.com>
2018-08-27 22:29:35 -0300

    Fixing Schema and rest method

--- src/api/user/user.ts
+++ src/api/user/user.ts
@@ -1,7 +1,8 @@
-import { BadRequestError } from 'error-middleware/errors'
+import { BadRequestError, UnauthorizedError } from 'error-middleware/errors'
 import { validationMiddleware } from 'error-middleware/middlewares'
 import * as express from 'express'
 import * as asyncHandler from 'express-async-handler'
+import * as jwt from 'jsonwebtoken'
 import { UserModel } from '../../models/user'
 
 const { checkSchema, validationResult } = require('express-validator/check')
@@ -25,6 +26,39 @@ const schema: any = {
       errorMessage: 'Password should be at least 5 chars long and max of 10 chars long',
       options: { min: 5, max: 10 },
     },
+  },
+  name: {
+    in: 'body',
+    isLength: {
+      errorMessage: 'name should be at least 3 chars long and max of 100 chars long',
+      options: { min: 3, max: 50 },
+    },
+  },
+}
+
+const editSchema: any = {
+  email: {
+    isEmail: true,
+    in: 'body',
+    trim: true,
+    errorMessage: 'Invalid email',
+    optional: true,
+  },
+  password: {
+    in: 'body',
+    isLength: {
+      errorMessage: 'Password should be at least 5 chars long and max of 10 chars long',
+      options: { min: 5, max: 10 },
+    },
+    optional: true,
+  },
+  name: {
+    in: 'body',
+    isLength: {
+      errorMessage: 'name should be at least 3 chars long and max of 100 chars long',
+      options: { min: 3, max: 50 },
+    },
+    optional: true,
   },
 }
 
@@ -52,31 +86,28 @@ router.post(
 
 router.patch(
   '/',
-  validationMiddleware(schema),
-  asyncHandler(async (req, res) => {
-    const user = await UserModel.getByEmail(req.body.email)
-    const data = {
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    }
-    if (!user) {
-      throw new BadRequestError({
-        error: 'User not found.',
-      })
-    }
-    if (req.body.password !== user.password) {
-      throw new BadRequestError({
-        error: 'Your credentials are invalid',
-      })
-    }
-    const updatedUser = await UserModel.update(user.userId, data)
-    if (user) {
+  validationMiddleware(editSchema),
+  verifyToken,
+  asyncHandler(async (req: any, res) => {
+    const user = req.authData.user.user
+    const data = req.body
+    const updatedUser = await UserModel.update(user.user_id, data)
+    if (updatedUser) {
       res.send('User updated successfully')
     } else {
       throw new BadRequestError()
     }
   }),
 )
+function verifyToken(req, res, next) {
+  const authToken = req.headers.authorization
+  jwt.verify(authToken, 'secretKeyHere', (err, authData) => {
+    if (err) {
+      throw new UnauthorizedError()
+    }
+    req.authData = authData
+    next()
+  })
+}
 
 export = router
