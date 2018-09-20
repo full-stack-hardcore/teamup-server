d172484ddc065a1cbcc9bff805ace353836c95f2
Lucas Nachtigall <lucas.nachtigall@gmail.com>
2018-09-20 20:07:00 -0300

    Cleaning code

--- src/middleware/authentication.middleware.test.ts
+++
@@ -1,9 +0,0 @@
-import { Login } from '../services/login.service'
-import { verifyToken } from './authentication.middleware'
-
-// ToDo
-describe('Authentication Middleware Tests', () => {
-  test('Tests Token Authentication', () => {
-    expect(true).toBe(true)
-  })
-})
